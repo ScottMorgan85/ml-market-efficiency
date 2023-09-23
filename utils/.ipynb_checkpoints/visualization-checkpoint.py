c37ad8da1c10dc297125a1c4e1e19e7b338b86b0
Scott Morgan <smm253@gmail.com>
2023-09-23 05:54:17 -0700

    Update to content

--- utils/.ipynb_checkpoints/visualization-checkpoint.py
+++ utils/.ipynb_checkpoints/visualization-checkpoint.py
@@ -152,9 +152,82 @@ def plot_correlation(index_returns, start_date, end_date):
     plt.title(title, fontsize=14)
     plt.show()
 
-    
-from IPython.display import display, HTML
-import plotly.graph_objects as go
+##Would not render interactive graph on nbviewer   
+# from IPython.display import display, HTML
+# import plotly.graph_objects as go
+
+# def plot_cumulative_returns_with_events(index_returns):
+#     cumulative_returns = (1 + index_returns).cumprod()
+
+#     # Extracting the dates and labels directly from df_events in Constants.py
+#     events_dates = df_events['Event Date']
+#     events_labels = df_events['Event'].tolist()
+
+#     # Define test periods based on events
+#     test_periods = [(date, date + pd.Timedelta(days=30)) for date in events_dates]
+
+#     # Create a new figure
+#     fig = go.Figure()
+
+#     # Plot cumulative returns for each asset
+#     for asset in asset_columns:
+#         fig.add_trace(go.Scatter(x=cumulative_returns.index, y=cumulative_returns[asset], mode='lines', name=asset))
+
+#         # Highlight events with shaded regions and label them
+#     for date, label in zip(events_dates, events_labels):
+#         # Shading 30 days post-event
+#         fig.add_vrect(x0=date, x1=date + pd.Timedelta(days=30), fillcolor="gray", opacity=0.2, line_width=0)
+#         # Annotating the event
+#         fig.add_annotation(x=date, y=max(cumulative_returns.loc[date]), text=label, showarrow=True, arrowhead=4, ax=0, ay=-40)
+
+#     # Annotate Test Periods
+#     for start, end in test_periods:
+#         mid_date = start + (end - start) / 2
+#         closest_date = cumulative_returns.index[np.argmin(np.abs(cumulative_returns.index - mid_date))]
+#         fig.add_annotation(
+#             x=closest_date, 
+#             y=cumulative_returns.min().min(),  # Set to minimum cumulative return for bottom
+#             text="Test Period", 
+#             showarrow=False, 
+#             font=dict(color="blue", size=10),
+#             textangle=90
+#         )
+
+#     # Annotate Train Periods
+#     train_start = cumulative_returns.index[0]
+#     for start, _ in test_periods:
+#         mid_date = train_start + (start - train_start) / 2
+#         closest_date = cumulative_returns.index[np.argmin(np.abs(cumulative_returns.index - mid_date))]
+#         fig.add_annotation(
+#             x=closest_date, 
+#             y=cumulative_returns.min().min(),  # Set to minimum cumulative return for bottom
+#             text="Train Period", 
+#             showarrow=False, 
+#             font=dict(color="red", size=10),
+#             textangle=90
+#         )
+#         train_start = start + pd.Timedelta(days=30)
+
+#     # Annotate last Train Period if necessary
+#     if train_start < cumulative_returns.index[-1]:
+#         mid_date = train_start + (cumulative_returns.index[-1] - train_start) / 2
+#         closest_date = cumulative_returns.index[np.argmin(np.abs(cumulative_returns.index - mid_date))]
+#         fig.add_annotation(
+#             x=closest_date, 
+#             y=cumulative_returns.min().min(),  # Set to minimum cumulative return for bottom
+#             text="Train Period", 
+#             showarrow=False, 
+#             font=dict(color="red", size=10),
+#             textangle=90
+#         )
+
+#     fig.update_layout(title='Cumulative Returns and Model Test/Train Periods', yaxis_title='Cumulative Return', height=600)
+#     fig.show()
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import pandas as pd
 
 def plot_cumulative_returns_with_events(index_returns):
     cumulative_returns = (1 + index_returns).cumprod()
@@ -166,63 +239,40 @@ def plot_cumulative_returns_with_events(index_returns):
     # Define test periods based on events
     test_periods = [(date, date + pd.Timedelta(days=30)) for date in events_dates]
 
-    # Create a new figure
-    fig = go.Figure()
-
+    fig, ax = plt.subplots(figsize=(10, 6))
+    
     # Plot cumulative returns for each asset
     for asset in asset_columns:
-        fig.add_trace(go.Scatter(x=cumulative_returns.index, y=cumulative_returns[asset], mode='lines', name=asset))
-
-        # Highlight events with shaded regions and label them
-    for date, label in zip(events_dates, events_labels):
-        # Shading 30 days post-event
-        fig.add_vrect(x0=date, x1=date + pd.Timedelta(days=30), fillcolor="gray", opacity=0.2, line_width=0)
-        # Annotating the event
-        fig.add_annotation(x=date, y=max(cumulative_returns.loc[date]), text=label, showarrow=True, arrowhead=4, ax=0, ay=-40)
+        ax.plot(cumulative_returns.index, cumulative_returns[asset], label=asset)
 
     # Annotate Test Periods
     for start, end in test_periods:
+        ax.axvspan(start, end, color="gray", alpha=0.2)
         mid_date = start + (end - start) / 2
-        closest_date = cumulative_returns.index[np.argmin(np.abs(cumulative_returns.index - mid_date))]
-        fig.add_annotation(
-            x=closest_date, 
-            y=cumulative_returns.min().min(),  # Set to minimum cumulative return for bottom
-            text="Test Period", 
-            showarrow=False, 
-            font=dict(color="blue", size=10),
-            textangle=90
-        )
+        ax.annotate("Test Period", (mid_date, cumulative_returns.min().min()), color="blue", rotation=90)
 
     # Annotate Train Periods
     train_start = cumulative_returns.index[0]
     for start, _ in test_periods:
         mid_date = train_start + (start - train_start) / 2
-        closest_date = cumulative_returns.index[np.argmin(np.abs(cumulative_returns.index - mid_date))]
-        fig.add_annotation(
-            x=closest_date, 
-            y=cumulative_returns.min().min(),  # Set to minimum cumulative return for bottom
-            text="Train Period", 
-            showarrow=False, 
-            font=dict(color="red", size=10),
-            textangle=90
-        )
+        ax.annotate("Train Period", (mid_date, cumulative_returns.min().min()), color="red", rotation=90)
         train_start = start + pd.Timedelta(days=30)
 
     # Annotate last Train Period if necessary
     if train_start < cumulative_returns.index[-1]:
         mid_date = train_start + (cumulative_returns.index[-1] - train_start) / 2
-        closest_date = cumulative_returns.index[np.argmin(np.abs(cumulative_returns.index - mid_date))]
-        fig.add_annotation(
-            x=closest_date, 
-            y=cumulative_returns.min().min(),  # Set to minimum cumulative return for bottom
-            text="Train Period", 
-            showarrow=False, 
-            font=dict(color="red", size=10),
-            textangle=90
-        )
+        ax.annotate("Train Period", (mid_date, cumulative_returns.min().min()), color="red", rotation=90)
+        
+    # Annotate events
+    for date, label in zip(events_dates, events_labels):
+        ax.annotate(label, (date, max(cumulative_returns.loc[date])), arrowprops=dict(arrowstyle="->"), xytext=(0,40), textcoords="offset points")
 
-    fig.update_layout(title='Cumulative Returns and Model Test/Train Periods', yaxis_title='Cumulative Return', height=600)
-    fig.show()
+    ax.set_title('Cumulative Returns and Model Test/Train Periods')
+    ax.set_ylabel('Cumulative Return')
+    ax.legend()
+    
+    plt.tight_layout()
+    plt.show()
 
 def display_event_table():
     # Creating the centered table directly using df_events from Constants.py
